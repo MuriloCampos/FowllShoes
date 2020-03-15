ad2af45806039cce8867f8bc60603e4cfb4d4bcb
Murilo Campos <murilo13campos@gmail.com>
2020-03-15 09:51:26 -0400

    styling the home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-feminino/79/D22-2018-879/D22-2018-879_detalhe1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito doido</strong>
+        <span>R$ 300</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-feminino/79/D22-2018-879/D22-2018-879_detalhe1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito doido</strong>
+        <span>R$ 300</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-feminino/79/D22-2018-879/D22-2018-879_detalhe1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito doido</strong>
+        <span>R$ 300</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-feminino/79/D22-2018-879/D22-2018-879_detalhe1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito doido</strong>
+        <span>R$ 300</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-feminino/79/D22-2018-879/D22-2018-879_detalhe1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito doido</strong>
+        <span>R$ 300</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-exact-feminino/79/D22-2018-879/D22-2018-879_detalhe1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis muito doido</strong>
+        <span>R$ 300</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
