2cd75153f2ead4e46d821f9b2507beea23f6fc68
Murilo Campos <murilo13campos@gmail.com>
2020-03-15 09:26:34 -0400

    styling the header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+export const Title = styled(Link)`
+  color: #fff;
+  font-weight: bold;
+  font-size: 30px;
+  text-decoration: none;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
